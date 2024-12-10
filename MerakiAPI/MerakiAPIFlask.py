069d578cfa56b613e17c24e112f7c37cadf3c79d
Bartesaghi Stefano <barte91@msn.com>
2024-12-10 13:41:36 +0100

    update

--- MerakiAPI/MerakiAPIFlask.py
+++ MerakiAPI/MerakiAPIFlask.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 URL = "https://api.meraki.com/api/v1"  # URL MERKI API
 APIKEY = {"X-Cisco-Meraki-API-Key": "f25d79a1df42dff69f5337fa61c60c2b798aa404"}  # API PER TECNOMAT
-orgID = "280759"  # TECNOMAT ITALIA
+#orgID = "280759"  # TECNOMAT ITALIA
 xls_path_inv = "C:\\MerakiAPI\\IntToShutInv_"
 xls_path_chgIP = "C:\\MerakiAPI\\ChangeIP"
 xls_path = "C:\\MerakiAPI"

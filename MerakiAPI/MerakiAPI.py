26b0a36bd61aa19e9dab6be4918e1eed6ca299ac
Bartesaghi Stefano <barte91@msn.com>
2025-11-27 16:30:57 +0100

    update-hidden psw

--- MerakiAPI/MerakiAPI.py
+++ MerakiAPI/MerakiAPI.py
@@ -4,17 +4,9 @@ from Inventario import *
 from UpdatePorts import *
 from ChangeIP import *
 from Tools import *
+from dotenv import load_dotenv
 
-
-URL="https://api.meraki.com/api/v1" # URL MERKI API
-APIKEY = {"X-Cisco-Meraki-API-Key": "f25d79a1df42dff69f5337fa61c60c2b798aa404"} # API PER TECNOMAT
-
-orgID = "280759" # TECNOMAT ITALIA
-xls_path_inv="C:\MerakiAPI\IntToShutInv_"
-xls_path_chgIP="C:\MerakiAPI\ChangeIP"
-xls_path="C:\MerakiAPI"
-json_script_path=r"\\192.168.100.65\Archivio Tecnico\Meraki API\SCRIPT\JSON"
-
+load_dotenv()
 
 if __name__ == '__main__':
     menu = ConsoleMenu("Scegli Operazione da eseguire")
